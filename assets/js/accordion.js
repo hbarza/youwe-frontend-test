64a1c2d983a850f82578b782c09b253fc38fcede
Omid <hbarza@gmail.com>
2019-02-13 09:47:34 +0330

    working on accordion

---
+++ assets/js/accordion.js
@@ -0,0 +1,24 @@
+/**
+ * Accordion plugin
+ *
+ * @author Omid Barza <hbarza@gmail.com>
+ */
+
+accordion = function(openFirst, autoClose) 
+{
+    /**
+     * Defines first element of accordion is opened automatically when creating accordion
+     * 
+     * @var {bool}
+     */
+    this.openFirst = openFirst || true;
+
+    /**
+     * Defines auto close for open elements is enabled when click on closed element to open
+     * 
+     * @var {bool}
+     */
+    this.autoClose = autoClose || true;
+
+    this.toggle
+}
